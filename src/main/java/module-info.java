5d7ad0ce4b11b1fdc1a219401f0b15ea1cefd854
方正 <1640691243@qq.com>
2023-04-08 11:03:50 +0800

    2023.4.8更新

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module demo{
     requires javacv;
     requires javafx.controls;
     requires javafx.graphics;
+    requires io.netty.transport;
+    requires io.netty.codec;
+    requires io.netty.handler;
+    requires io.netty.common;
+    requires io.netty.buffer;
+    requires oshi.core;
 
     opens com.example to javacv,javafx.controls,java.desktop,javafx.graphics;
     exports com.example;
